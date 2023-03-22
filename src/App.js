e4881f8e8c733304f36157174f54428d41e7a8f8
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-21 22:39:35 -0500

    added a few routes and tested

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
+
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => {
-  return <div>App</div>;
+  return <Router>
+    {/* Switch works by ensuring that if the url matches on of the route paths, then nothing else will be read */}
+    <Switch>
+    {/* Putting exact makes sure that whatever is inside of route will only be rendered with the EXACT path */}
+    <Route path="/" exact>
+      <Users />
+    </Route>
+    <Route path="/places/new" exact>
+      <NewPlace />
+    </Route>
+    <Redirect to="/"/>
+    </Switch>
+  </Router>;
 };
 
 export default App;
