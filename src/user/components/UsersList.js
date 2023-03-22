e274af6e24440567189eadbb2ca5b988dd67ce66
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-21 23:05:18 -0500

    Components for User List and each User Item created

---
+++ src/user/components/UsersList.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import UserItem from "./UserItem";
+import "./UsersList.css";
+
+const UsersList = ({ items }) => {
+  if (items.length === 0) {
+    return (
+      <div className="center">
+        <h2>No Users Found.</h2>
+      </div>
+    );
+  }
+
+  return (
+    <ul className="users-list">
+      {items.map((user) => {
+        return (
+          <UserItem
+            key={user.id}
+            id={user.id}
+            image={user.image}
+            name={user.name}
+            placeCount={user.places}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+export default UsersList;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    // {
+    //   id: "u1",
+    //   name: "Diante Dowdell",
+    //   image:
+    //     "https://goodsmileshop.com/medias/sys_master/images/images/h38/hd0/9373578788894.jpg",
+    //   places: 3,
+    // },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
