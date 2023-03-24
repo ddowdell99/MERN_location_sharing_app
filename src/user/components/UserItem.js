5e2dabba9370b50a46e9f04e99fff13500c90ead
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-23 23:13:58 -0500

    adding user card

--- src/user/components/UserItem.js
+++ src/user/components/UserItem.js
@@ -1,19 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
+import Avatar from "../../shared/components/UIElements/Avatar";
+import Card from "../../shared/components/UIElements/Card";
 import "./UserItem.css";
 
-const UserItem = ({ image, name, placeCount }) => {
-  return <li className="user-item">
-    <div className="user-item__content"> 
+const UserItem = ({ image, name, placeCount, id }) => {
+  return (
+    <li className="user-item">
+        <Card className="user-item__content">
+        <Link to={`/${id}/places`}>
         <div className="user-item__image">
-            <img src={image} alt={name}/>
+          <Avatar image={image} alt={name} />
         </div>
         <div className="user-item__info">
-    <h2>{name}</h2>
-    <h3>{placeCount} {placeCount === 1 ? 'Place' : 'Places'}</h3>
+          <h2>{name}</h2>
+          <h3>
+            {placeCount} {placeCount === 1 ? "Place" : "Places"}
+          </h3>
         </div>
-    </div>
-  </li>;
+        </Link>
+        </Card>
+    </li>
+  );
 };
 
 export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -4,13 +4,13 @@ import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
-    // {
-    //   id: "u1",
-    //   name: "Diante Dowdell",
-    //   image:
-    //     "https://goodsmileshop.com/medias/sys_master/images/images/h38/hd0/9373578788894.jpg",
-    //   places: 3,
-    // },
+    {
+      id: "u1",
+      name: "Diante Dowdell",
+      image:
+        "https://goodsmileshop.com/medias/sys_master/images/images/h38/hd0/9373578788894.jpg",
+      places: 3,
+    },
   ];
 
   return <UsersList items={USERS} />;
