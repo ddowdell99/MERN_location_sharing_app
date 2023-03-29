2b03786276746a1550e386f2da7ea820cc8de9f6
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-03-28 22:46:22 -0500

    Filtering places by userID using useParams()

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,36 +1,43 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 import PlaceList from "../components/PlaceList";
 
 const DUMMY_PLACES = [
-    {
-        id: 'p1',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St., New York, NY 10001', 
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        }, 
-        creator: 'u1'
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
     },
-    {
-        id: 'p2',
-        title: 'Empire State Building',
-        description: 'One of the most famous sky scrapers in the world!',
-        imageURL: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg',
-        address: '20 W 34th St., New York, NY 10001', 
-        location: {
-            lat: 40.7484405,
-            lng: -73.9856644
-        }, 
-        creator: 'u2'
-    }
-]
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world!",
+    imageURL:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/800px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St., New York, NY 10001",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    creator: "u2",
+  },
+];
 
 const UserPlaces = () => {
-  return <PlaceList items={DUMMY_PLACES}/>
+  // useParams is used here to grab the user ID from the dynamic url for seeing specific user's places
+  const userID = useParams().userID;
+//   Using .filter to take out all the places that match the creator id
+  const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userID)
+  return <PlaceList items={loadedPlaces} />;
 };
 
 export default UserPlaces;
