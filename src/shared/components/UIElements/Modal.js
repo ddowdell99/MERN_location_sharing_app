e8c1cad711acb234f7231e3a2e0e9b5fe13861b9
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-04-03 21:51:57 -0500

    started creating shared modal template to use for maps and signing in

---
+++ src/shared/components/UIElements/Modal.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
+
+import Backdrop from "./Backdrop";
+import "./Modal.css";
+
+const ModalOverlay = ({
+  className,
+  style,
+  headerClass,
+  header,
+  onSubmit,
+  contentClass,
+  children,
+  footerClass,
+  footer,
+}) => {
+  const content = (
+    // adding the literal here gives us the chance to include other classNames/css styling depending on where we are using modal
+    <div className={`modal ${className}`} style={style}>
+      <header className={`modal__header ${headerClass}`}>
+        <h2>{header}</h2>
+      </header>
+      <form onSubmit={onSubmit ? onSubmit : (event) => event.preventDefault}>
+        <div className={`modal__content ${contentClass}`}>{children}</div>
+        <footer className={`modal__footer ${footerClass}`}>{footer}</footer>
+      </form>
+    </div>
+  );
+  return createPortal(content, document.getElementById("modal-hook"));
+};
+
+const Modal = ({ show, onCancel }) => {
+  return (
+    <React.Fragment>
+      {show && <Backdrop onClick={onCancel} />}
+      <CSSTransition
+        in={show}
+        mountOnEnter
+        unmountOnExit
+        timeout={200}
+        classNames="modal"
+      ></CSSTransition>
+    </React.Fragment>
+  );
+};
+
+export default Modal;
