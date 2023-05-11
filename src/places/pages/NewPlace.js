20cb1f650e9ef6cce8b5ea85648764bd98cd1545
Diante Dowdell <113080355+ddowdell99@users.noreply.github.com>
2023-05-10 22:54:48 -0500

    added more form validity and button based off it

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,14 +1,70 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useReducer } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
-import { VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import Button from "../../shared/components/FormElements/Button";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 import "./NewPlace.css";
 
-const NewPlace = () => {
-  // useCallback was used here to stop an infinite loop from rerendering this page and then forcing input useEffect to rerender
-  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case "INPUT_CHANGE":
+      let formIsValid = true;
+      for (const inputId in state.inputs) {
+        // Looking to see if the current inputId is the same as the inputId (action.inputId) we are trying to change
+        if (inputId === action.inputId) {
+          // formIsValid will be set to true as long as formIsValid and action.isValid is true
+          formIsValid = formIsValid && action.isValid;
+          // If not inputId we are looking at is not the input we are trying to change (title or description)
+        } else {
+          formIsValid = formIsValid && state.inputs[inputId].isValid;
+        }
+      }
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          // The bracket notation here adds a dynamic assignment where it will be updated based on what the inputId is (either
+          // title or description)
+          [action.inputId]: { value: action.value, isValid: action.isValid },
+        },
+        isValid: formIsValid,
+      };
 
-  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+    default:
+      return state;
+  }
+};
+
+const NewPlace = () => {
+  const [formState, dispatch] = useReducer(formReducer, {
+    inputs: {
+      title: {
+        value: "",
+        isValid: false,
+      },
+      description: {
+        value: "",
+        isValid: false,
+      },
+    },
+    isValid: false,
+  });
+
+  // useCallback was used here to stop an infinite loop from rerendering this page and then forcing input useEffect to rerender
+  const inputHandler = useCallback(
+    (id, value, isValid) => {
+      dispatch({
+        type: "INPUT_CHANGE",
+        value: value,
+        isValid: isValid,
+        inputId: id,
+      });
+    },
+    [dispatch]
+  );
 
   return (
     <form className="place-form">
@@ -19,7 +75,7 @@ const NewPlace = () => {
         element="input"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
-        onInput={titleInputHandler}
+        onInput={inputHandler}
       />
       <Input
         id="description"
@@ -27,8 +83,9 @@ const NewPlace = () => {
         label="Description"
         validators={[VALIDATOR_MINLENGTH(5)]}
         errorText="Please enter a valid description (at least 5 characters)."
-        onInput={descriptionInputHandler}
+        onInput={inputHandler}
       />
+      <Button type="submit" disabled={!formState.isValid}>ADD PLACE</Button>
     </form>
   );
 };
